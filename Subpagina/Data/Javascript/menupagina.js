b1b01ad5ad060eed732ad216708c5f1a371cd97d
Bjorn <89516@glr.nl>
2023-03-23 16:04:49 +0100

    koop product werkt

--- Subpagina/Data/Javascript/menupagina.js
+++ Subpagina/Data/Javascript/menupagina.js
@@ -80,7 +80,146 @@ if (product === 'blackBass') {
     totaalBlackbass = totaalBlackbass + aantal;
 }
 
-document.getElementById("aantalArtichoke").value = artichoke;
-document.getElementById("aantalBlackBass").value = blackbass;
+if (product === 'caviar') {
+    caviar = caviar + 1;
+    totaalCaviar = totaalCaviar + aantal;
 }
 
+if (product === 'crab') {
+    crab = crab + 1;
+    totaalCrab = totaalCrab + aantal;
+}
+
+if (product === 'filetMignon') {
+    filetMignon = filetMignon + 1;
+    totaalFiletmignon = totaalFiletmignon + aantal;
+}
+
+if (product === 'kampachi') {
+    kampachi = kampachi + 1;
+    totaalKampachi = totaalKampachi + aantal;
+}
+
+if (product === 'langoustine') {
+    langoustine = langoustine + 1;
+    totaalLangoustine = totaalLangoustine + aantal;
+}
+
+if (product === 'lobster') {
+    lobster = lobster + 1;
+    totaalLobster = totaalLobster + aantal;
+}
+
+if (product === 'mesclunSalad') {
+    mesclunsalad = mesclunsalad + 1;
+    totaalMesclunsalad = totaalMesclunsalad + aantal;
+}
+
+if (product === 'octopus') {
+    octopus = octopus + 1;
+    totaalOctopus = totaalOctopus + aantal;
+}
+
+if (product === 'oysterUni') {
+    oysteruni = oysteruni + 1;
+    totaalOysteruni = totaalOysteruni + aantal;
+}
+
+if (product === 'oyster') {
+    oyster = oyster + 1;
+    totaalOyster = totaalOyster + aantal;
+}
+
+if (product === 'redSnapper') {
+    redsnapper = redsnapper + 1;
+    totaalRedsnapper = totaalRedsnapper + aantal;
+}
+
+if (product === 'salmon') {
+    salmon = salmon + 1;
+    totaalSalmon = totaalSalmon + aantal;
+}
+
+if (product === 'scallopCaviar') {
+    scallopcaviar = scallopcaviar + 1;
+    totaalScallopcaviar = totaalScallopcaviar + aantal;
+}
+
+if (product === 'scallop') {
+    scallop = scallop + 1;
+    totaalScallop = totaalScallop + aantal;
+}
+
+if (product === 'shellfishMedley') {
+    shellfishmedley = shellfishmedley + 1;
+    totaalShellfishmedley = totaalShellfishmedley + aantal;
+}
+
+if (product === 'tasmanianSeaTrout') {
+    tasmanianseatrout = tasmanianseatrout + 1;
+    totaalTasmanianseatrout = totaalTasmanianseatrout + aantal;
+}
+
+if (product === 'tuna') {
+    tuna = tuna + 1;
+    totaalTuna = totaalTuna + aantal;
+}
+
+if (product === 'tunaUrchin') {
+    tunaurchin = tunaurchin + 1;
+    totaalTunaurchin = totaalTunaurchin + aantal;
+}
+
+if (product === 'tapas') {
+    tapas = tapas + 1;
+    totaalTapas = totaalTapas + aantal;
+}
+
+document.getElementById("aantalArtichoke").value = artichoke;
+document.getElementById("aantalBlackBass").value = blackbass;
+document.getElementById("aantalCaviar").value = caviar;
+document.getElementById("aantalCrab").value = crab;
+document.getElementById("aantalFiletMignon").value = filetMignon;
+document.getElementById("aantalKampachi").value = kampachi;
+document.getElementById("aantalLangoustine").value = langoustine;
+document.getElementById("aantalLobster").value = lobster;
+document.getElementById("aantalMesclunSalad").value = mesclunsalad;
+document.getElementById("aantalOctopus").value = octopus;
+document.getElementById("aantalOysterUni").value = oysteruni;
+document.getElementById("aantalOyster").value = oyster;
+document.getElementById("aantalRedSnapper").value = redsnapper;
+document.getElementById("aantalSalmon").value = salmon;
+document.getElementById("aantalScallopCaviar").value = scallopcaviar;
+document.getElementById("aantalScallop").value = scallop;
+document.getElementById("aantalShellfishMedley").value = shellfishmedley;
+document.getElementById("aantalTasmanianSeaTrout").value = tasmanianseatrout;
+document.getElementById("aantalTuna").value = tuna;
+document.getElementById("aantalTunaUrchin").value = tunaurchin;
+document.getElementById("aantalTapas").value = tapas;
+
+document.getElementById("prijsArtichoke").value = totaalArtichoke;
+document.getElementById("prijsBlackBass").value = totaalBlackbass;
+document.getElementById("prijsCaviar").value = totaalCaviar;
+document.getElementById("prijsCrab").value = totaalCrab;
+document.getElementById("prijsFiletMignon").value = totaalFiletmignon;
+document.getElementById("prijsKampachi").value = totaalKampachi;
+document.getElementById("prijsLangoustine").value = totaalLangoustine;
+document.getElementById("prijsLobster").value = totaalLobster;
+document.getElementById("prijsMeslunSalad").value = totaalMesclunsalad;
+document.getElementById("prijsOctopus").value = totaalOctopus;
+document.getElementById("prijsOysterUni").value = totaalOysteruni;
+document.getElementById("prijsOyster").value = totaalOyster;
+document.getElementById("prijsRedSnapper").value = totaalRedsnapper;
+document.getElementById("prijsSalmon").value = totaalSalmon;
+document.getElementById("prijsScallopCaviar").value = totaalScallopcaviar;
+document.getElementById("prijsScallop").value = totaalScallop;
+document.getElementById("prijsShellfishMedley").value = totaalShellfishmedley;
+document.getElementById("prijsTasmanianSeaTrout").value = totaalTasmanianseatrout;
+document.getElementById("prijsTuna").value = totaalTuna;
+document.getElementById("prijsTunaUrchin").value = totaalTunaurchin;
+document.getElementById("prijsTapas").value = totaalTapas;
+}
+
+let verwijderProduct = function(product, aantal) {
+
+}
